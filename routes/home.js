535feb4773b4822d6bd5f4705d11514ec9bea023
Maxvo <maxvo25@gmail.com>
2024-02-04 22:46:51 +1030

    merge file api no longer upload final video, this one to remove timeout error on vercel

--- routes/home.js
+++ routes/home.js
@@ -1,23 +1,19 @@
+require('dotenv').config();
 const express = require("express");
 const router = express.Router();
-require('dotenv').config();
 const request = require('request-promise');
 const fs = require('fs');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegPath);
 const { OpenAI } = require('openai');
-const http = require('http');
 const { createApi } = require('unsplash-js');
-const { put, list } = require("@vercel/blob");
-const stream = require('stream');
 const path = require('path');
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config(JSON.parse(process.env.CLOUDINARY_CONFIG));
 
 const unsplash = createApi({ accessKey: process.env.UNSPLASH_ACCESS_KEY });
-
 
 const tempPath = '/tmp/';
 const finalVideoPath = path.join(tempPath, 'final.mp4');
@@ -29,10 +25,7 @@ const API_ENDPOINT = 'https://audio.api.speechify.com/generateAudioFiles';
 const payload = {
     audioFormat: "mp3",
     paragraphChunks: ["Script HERE"],
-    voiceParams: {
-        "name": "PVL:3371c48a-a894-47f3-8100-b3028f1fcf4b", "engine": "speechify",
-        "languageCode": "en-US"
-    }
+    voiceParams: JSON.parse(process.env.SPEECHIFY_VOICE_PARAMS)
 };
 
 const uploadToCloudinary = async (filePath, fileBuffer, fileType, isImage = true) => {
@@ -66,11 +59,10 @@ const generateSingleVideo = function (tag, audioData, imageData) {
         let outputFilePath = `temp${tag}.mp4`;
         let imageFile = `image${tag}.png`;
         let audioFile = `audio${tag}.mp3`;
-        // fs.writeFileSync(audioFile, audioData);
-        // fs.writeFileSync(imageFile, imageData);
+
         const imageUrl = await uploadToCloudinary(imageFile, imageData, 'image/png');
         const audioUrl = await uploadToCloudinary(audioFile, audioData, 'audio/mp3', false);
-        // Create a new FFmpeg command
+
         const command = ffmpeg();
         command.input(imageUrl);
         command.inputFormat('image2');
@@ -114,18 +106,6 @@ const generateSingleVideo = function (tag, audioData, imageData) {
             .run();
     })
 }
-
-router.get('/final-video', async (req, res) => {
-    let filePath = path.join(tempPath, `${req.query.filename}.mp4`);
-    const stat = fs.statSync(filePath);
-    const fileSize = stat.size;
-    const head = {
-        'Content-Length': fileSize,
-        'Content-Type': 'video/mp4',
-    };
-    res.writeHead(200, head);
-    fs.createReadStream(filePath).pipe(res);
-})
 
 const fetchVideoDetails = (publicIds) => {
     return new Promise((resolve, reject) => {
@@ -186,9 +166,10 @@ router.post('/merge-video', async (req, res) => {
         .output(finalVideoPath)
         .on('end', async () => {
             const videoData = fs.readFileSync(`/tmp/final.mp4`);
-            await uploadToCloudinary('final.mp4', videoData, 'video/mp4', false);
+            const videoBase64 = videoData.toString('base64');
+            // await uploadToCloudinary('final.mp4', videoData, 'video/mp4', false);
             console.log('Video merging finished.');
-            res.json({ success: true });
+            res.json({ success: true, data: videoBase64 });
         })
         .on('error', (err) => {
             console.error('Error:', err);
@@ -260,97 +241,11 @@ router.get('/generate-script', async (req, res) => {
     if (response.choices && response.choices.length > 0) {
         // Assuming you want the first completion
         const result = response.choices[0].message.content;
-        console.log("Chat completion:", result);
         res.json(JSON.parse(result));
     } else {
         console.log("No completion found in the response");
         res.json(null);
     }
 });
-
-
-router.post('/script-to-video', async (req, res) => {
-    const videoIndex = req.body.videoData;
-    const videoData = req.body.videoData;
-    scriptToVideo(videoData)
-        .then(resp => {
-            if (!resp)
-                res.json({ success: false });
-            else {
-                http.createServer(function (req, res) {
-                    // Set the correct headers for video streaming
-                    res.writeHead(200, { 'Content-Type': 'video/mp4' });
-                    // Create a stream for the video file and pipe it to the response
-                    const stream = fs.createReadStream(finalVideoPath);
-                    stream.pipe(res);
-                }).listen(8000);
-
-                res.json({ success: true });
-            }
-        })
-})
-
-router.get('/test', async (req, res) => {
-    const tag = 0;
-    let outputFilePath = `${tempPath}temp${tag}.mp4`;
-    const imageFile = `${tempPath}image${tag}.png`;
-    const audioFile = `${tempPath}audio${tag}.mp3`;
-
-    const audioUrl = 'https://res.cloudinary.com/dkx4jbkdu/video/upload/v1707031930/wyj93c0a8hzl5xf71nfr.mp3';
-    const imageUrl = 'https://res.cloudinary.com/dkx4jbkdu/image/upload/v1707031932/f5ddgdumltflklydid93.jpg';
-
-    // Create a new FFmpeg command
-    const command = ffmpeg();
-    command.input(imageUrl);
-    command.inputFormat('image2');
-    command.input(audioUrl);
-    command.inputFormat('mp3')
-
-    const tempOutputPath = path.join('/tmp/', 'test0.mp4'); // Temporary file
-
-    const passThrough = new stream.PassThrough();
-
-    // Specify output options
-    command
-        .fps(30)
-        .addOptions([
-            '-pix_fmt yuv420p',
-            // '-vf scale=1920:1080,setsar=1',
-            '-vf scale=1024:1024,setsar=1',
-            '-c:v libx264',
-            '-c:a aac',
-            '-strict experimental',
-            '-ar 44100',
-            '-r 30',
-            '-crf 23',
-            '-tune stillimage',
-        ])
-        .output(tempOutputPath)
-
-    // Run the FFmpeg command
-    command
-        .on('end', async () => {
-            console.log('Done');
-
-            fs.stat(tempOutputPath, (err, stat) => {
-                const head = {
-                    'Content-Length': stat.size,
-                    'Content-Type': 'video/mp4',
-                };
-                res.writeHead(200, head);
-                fs.createReadStream(tempOutputPath).pipe(res);
-            });
-        })
-        .on('error', (err) => {
-            console.error('Error:', err);
-        })
-        .on('progress', (progress) => {
-            // You can also listen for progress events to get information on the compilation progress
-            console.log('Processing: ' + progress.percent + '% done');
-        })
-        .run();
-
-});
-
 
 module.exports = router;
