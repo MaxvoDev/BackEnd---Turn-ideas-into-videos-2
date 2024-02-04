bfa17216b59ffabaf3befb62ef9da924ed14fe80
Maxvo <maxvo25@gmail.com>
2024-02-04 19:37:27 +1030

    add api for playing video

--- routes/home.js
+++ routes/home.js
@@ -11,13 +11,14 @@ const http = require('http');
 const { createApi } = require('unsplash-js');
 const { put, list } = require("@vercel/blob");
 const stream = require('stream');
+const path = require('path');
 
 
-const unsplash = createApi({ accessKey: '80zvD8BgnZK7mussaC5qBFj9XGibs16W_PxRnTuhjcE' });
+const unsplash = createApi({ accessKey: 'axgDaWdQxno1ImdajxDnpJAp-QYGkoVOGuwJyOH1SuU' });
 
 
 const tempPath = '/tmp/';
-const finalVideoPath = '/tmp/final.mp4';
+const finalVideoPath = path.join(tempPath, 'final.mp4');
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
@@ -32,29 +33,23 @@ const payload = {
     }
 };
 
-const writeFile = async function (fileName, fileData) {
-    return await put(fileName, fileData, { access: 'public' });
-}
-
 const generateSingleVideo = function (tag, audioData, imageData) {
     return new Promise(async (resolve, reject) => {
-        let outputFilePath = `${tempPath}temp${tag}.mp4`;
-        const imageFile = `${tempPath}image${tag}.png`;
-        const audioFile = `${tempPath}audio${tag}.mp3`;
+        let outputFilePath = path.join(tempPath, `temp${tag}.mp4`);
+        let imageFile = path.join(tempPath, `image${tag}.mp4`);
+        let audioFile = path.join(tempPath, `audio${tag}.mp4`);
+        // let outputFilePath = `${tempPath}temp${tag}.mp4`;
+        // const imageFile = `${tempPath}image${tag}.png`;
+        // const audioFile = `${tempPath}audio${tag}.mp3`;
 
-        // fs.writeFileSync(audioFile, audioData);
-        // fs.writeFileSync(imageFile, imageData);
-        const audioUrl = await put(audioFile, audioData, { access: 'public' });
-        const imageUrl = await put(imageFile, imageData, { access: 'public' })
+        fs.writeFileSync(audioFile, audioData);
+        fs.writeFileSync(imageFile, imageData);
 
         // Create a new FFmpeg command
         const command = ffmpeg();
-        command.input(imageUrl.downloadUrl);
-        command.inputFormat('image2');
-        command.input(audioUrl.downloadUrl);
-        command.inputFormat('mp3')
+        command.input(imageFile);
+        command.input(audioFile);
 
-        const passThrough = new stream.PassThrough();
 
         // Specify output options
         command
@@ -62,7 +57,7 @@ const generateSingleVideo = function (tag, audioData, imageData) {
             .addOptions([
                 '-pix_fmt yuv420p',
                 // '-vf scale=1920:1080,setsar=1',
-                '-vf scale=1024:1024,setsar=1',
+                '-vf scale=1280:720,setsar=1',
                 '-c:v libx264',
                 '-c:a aac',
                 '-strict experimental',
@@ -71,14 +66,12 @@ const generateSingleVideo = function (tag, audioData, imageData) {
                 '-crf 23',
                 '-tune stillimage',
             ])
-            .output(passThrough)
-            .outputFormat('mp4');
+            .output(outputFilePath);
 
         // Run the FFmpeg command
         command
             .on('end', async () => {
-                await put(outputFilePath, passThrough, { access: 'public' })
-                resolve(outputFilePath);
+                resolve(true);
             })
             .on('error', (err) => {
                 console.error('Error:', err);
@@ -149,6 +142,17 @@ const scriptToVideo = function (videoData) {
 
 }
 
+router.get('/final-video', async(req, res) => {
+    const stat = fs.statSync(finalVideoPath);
+    const fileSize = stat.size;
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(finalVideoPath).pipe(res);
+})
+
 router.post('/merge-video', async (req, res) => {
     const videoLength = req.body.videoLen;
 
@@ -157,7 +161,7 @@ router.post('/merge-video', async (req, res) => {
 
     const inputFiles = [];
     for (let i = 0; i < videoLength; i++) {
-        let inputFile = `${tempPath}temp${i}.mp4`;
+        let inputFile = path.join(tempPath, `temp${i}.mp4`);
         inputFiles.push(inputFile);
         command.input(inputFile);
     }
@@ -178,7 +182,6 @@ router.post('/merge-video', async (req, res) => {
         .output(finalVideoPath)
         .on('end', () => {
             console.log('Video merging finished.');
-
             res.json({ success: true });
         })
         .on('error', (err) => {
@@ -199,7 +202,7 @@ router.post('/generate-video', async (req, res) => {
         perPage: 1
     })
         .then(resp => {
-            return request.get({ url: resp.response.results[0].urls.full, encoding: null });
+            return request.get({ url: `${resp.response.results[0].urls.raw}&w=1280&h=720&fit=crop`, encoding: null });
         })
 
     const audioScript = videoData.script;
@@ -259,92 +262,5 @@ router.get('/generate-script', async (req, res) => {
     }
 });
 
-
-router.post('/script-to-video', async (req, res) => {
-    const videoIndex = req.body.videoData;
-    const videoData = req.body.videoData;
-    scriptToVideo(videoData)
-        .then(resp => {
-            if (!resp)
-                res.json({ success: false });
-            else {
-                http.createServer(function (req, res) {
-                    // Set the correct headers for video streaming
-                    res.writeHead(200, { 'Content-Type': 'video/mp4' });
-                    // Create a stream for the video file and pipe it to the response
-                    const stream = fs.createReadStream(finalVideoPath);
-                    stream.pipe(res);
-                }).listen(8000);
-
-                res.json({ success: true });
-            }
-        })
-})
-
-const path = require('path');
-
-router.get('/test', async (req, res) => {
-    const tag = 0;
-    let outputFilePath = `${tempPath}temp${tag}.mp4`;
-    const imageFile = `${tempPath}image${tag}.png`;
-    const audioFile = `${tempPath}audio${tag}.mp3`;
-
-    // fs.writeFileSync(audioFile, audioData);
-    // fs.writeFileSync(imageFile, imageData);
-    const audioUrl = 'https://res.cloudinary.com/dkx4jbkdu/video/upload/v1707031930/wyj93c0a8hzl5xf71nfr.mp3';
-    const imageUrl = 'https://res.cloudinary.com/dkx4jbkdu/image/upload/v1707031932/f5ddgdumltflklydid93.jpg';
-
-    // Create a new FFmpeg command
-    const command = ffmpeg();
-    command.input(imageUrl);
-    command.inputFormat('image2');
-    command.input(audioUrl);
-    command.inputFormat('mp3')
-
-    const tempOutputPath = path.join('/tmp/', 'tempOutput.mp4'); // Temporary file
-
-    const passThrough = new stream.PassThrough();
-
-    // Specify output options
-    command
-        .fps(30)
-        .addOptions([
-            '-pix_fmt yuv420p',
-            // '-vf scale=1920:1080,setsar=1',
-            '-vf scale=1024:1024,setsar=1',
-            '-c:v libx264',
-            '-c:a aac',
-            '-strict experimental',
-            '-ar 44100',
-            '-r 30',
-            '-crf 23',
-            '-tune stillimage',
-        ])
-        .output(tempOutputPath)
-
-    // Run the FFmpeg command
-    command
-        .on('end', async () => {
-            console.log('Done');
-
-            fs.stat(tempOutputPath, (err, stat) => {
-                const head = {
-                    'Content-Length': stat.size,
-                    'Content-Type': 'video/mp4',
-                };
-                res.writeHead(200, head);
-                fs.createReadStream(tempOutputPath).pipe(res);
-            });
-        })
-        .on('error', (err) => {
-            console.error('Error:', err);
-        })
-        .on('progress', (progress) => {
-            // You can also listen for progress events to get information on the compilation progress
-            console.log('Processing: ' + progress.percent + '% done');
-        })
-        .run();
-
-});
 
 module.exports = router;
